f29fe6c0b0e1350f6deda10df446dc0c9f2af2a1
Imran Nazir <imran.web.developer2021@gmail.com>
2023-06-07 20:29:47 +0600

    add loop

--- pep.js
+++ pep.js
@@ -5,85 +5,98 @@ const puppeteer = require("puppeteer");
     headless: false,
     userDataDir: "data",
   });
-
-  await page.goto("https://www.gsmarena.com/samsung_galaxy_s23+-12083.php", {
+  const page = await browser.newPage();
+  await page.goto("https://www.gsmarena.com/samsung-phones-9.php", {
     timeout: 0,
   });
 
-  const data = await page.evaluate(() => {
-    const title = document.querySelector(".specs-phone-name-title").textContent;
-    const brand = title.split(" ")[0];
-    const regexPattern = new RegExp(`${brand}\\s`, "i");
-    const model = title.replace(regexPattern, "");
-    const model_id = title.split(" ").join("_").toLowerCase();
-    const category = "";
-    let variants;
-    const image_url = "";
-    const content = {};
-    let status = "upcoming";
-    let approved = false;
+  await page.waitForSelector(".makers");
 
-    const tableBodies = document.querySelectorAll("table > tbody");
+  const productLinks = await page.$$eval(".makers a", (links) =>
+    links.map((link) => link.href)
+  );
+  for (const link of productLinks) {
+    await page.goto(link, { timeout: 0 });
+    const data = await page.evaluate(() => {
+      const title = document.querySelector(
+        ".specs-phone-name-title"
+      ).textContent;
+      const brand = title.split(" ")[0];
+      const regexPattern = new RegExp(`${brand}\\s`, "i");
+      const model = title.replace(regexPattern, "");
+      const model_id = title.split(" ").join("_").toLowerCase();
+      const category = "";
+      let variants;
+      const image_url = "";
+      const content = {};
+      let status = "upcoming";
+      let approved = true;
 
-    for (const body of tableBodies) {
-      const rows = body.querySelectorAll("tr");
+      const tableBodies = document.querySelectorAll("table > tbody");
 
-      let contentHeading;
+      for (const body of tableBodies) {
+        const rows = body.querySelectorAll("tr");
 
-      const tableContent = {};
+        let contentHeading;
 
-      for (const row of rows) {
-        const th = row.querySelector("th");
-        const td = row.querySelector("td.ttl");
+        const tableContent = {};
 
-        if (th) {
-          const heading = th.textContent;
-          contentHeading = heading;
-        }
+        for (const row of rows) {
+          const th = row.querySelector("th");
+          const td = row.querySelector("td.ttl");
 
-        if (td) {
-          const subHeadingContent = td.textContent;
-          const tbContent = row.querySelector(".nfo");
+          if (th) {
+            const heading = th.textContent;
+            contentHeading = heading;
+          }
 
-          if (tbContent) {
-            const textContent = tbContent.textContent;
+          if (td) {
+            const subHeadingContent = td.textContent;
+            const tbContent = row.querySelector(".nfo");
 
-            if (subHeadingContent === "Internal") {
-              const deviceSpecs = textContent
-                .split(", ")
-                .map((spec) => spec.replace("GB", "").replace("TB", "000"));
+            if (tbContent) {
+              const textContent = tbContent.textContent;
 
-              variants = deviceSpecs.map((spec) => {
-                const [rom, ram] = spec.split(" ");
-                return { ROM: parseInt(rom), RAM: parseInt(ram), price: 0 };
-              });
+              if (subHeadingContent === "Internal") {
+                const deviceSpecs = textContent
+                  .split(", ")
+                  .map((spec) => spec.replace("GB", "").replace("TB", "000"));
+
+                variants = deviceSpecs.map((spec) => {
+                  const [rom, ram] = spec.split(" ");
+                  return { ROM: parseInt(rom), RAM: parseInt(ram), price: 0 };
+                });
+              }
+
+              tableContent[
+                subHeadingContent.split(" ").join("_").toLowerCase()
+              ] = textContent;
             }
-
-            tableContent[subHeadingContent.split(" ").join("_").toLowerCase()] =
-              textContent;
           }
         }
+
+        if (contentHeading) {
+          content[contentHeading.split(" ").join("_")] = tableContent;
+        }
       }
 
-      if (contentHeading) {
-        content[contentHeading.split(" ").join("_")] = tableContent;
-      }
-    }
+      return {
+        title,
+        brand,
+        model,
+        model_id,
+        category,
+        variants,
+        status,
+        approved,
+        image_url,
+        content,
+      };
+    });
 
-    return {
-      title,
-      brand,
-      model,
-      model_id,
-      category,
-      variants,
-      status,
-      approved,
-      image_url,
-      content,
-    };
-  });
+    await page.goBack();
+    console.log(data.title);
+  }
 
-  console.log(data.title);
   await browser.close();
 })();
