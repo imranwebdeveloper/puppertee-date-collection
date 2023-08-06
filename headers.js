83f06a240576882916ab384e37d0a08104471d19
Imran Nazir <imran.web.developer2021@gmail.com>
2023-08-06 16:46:50 +0600

    update

--- headers.js
+++
@@ -1,8 +0,0 @@
-const headers = {
-  headers: {
-    "x-api-key":
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImphIiwibmFtZSI6ImltcmFuIiwiZG9tYWluIjoibW9iaWxlc2VsbGVyYmQuY29tIiwiaWF0IjoxNjg0MjE5NjA0fQ.Nc22sGygtB-5smG1OqeWBocmG-tJyDFhkCPpeudwnx4",
-  },
-};
-
-module.exports = headers;
--- single.js
+++ single.js
@@ -1,5 +1,4 @@
 const puppeteer = require("puppeteer");
-const { headers } = require("./headers");
 const axios = require("axios").default;
 
 (async () => {
